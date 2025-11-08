87d7a3df78889bd8bf541ec4617df8df5ad10177
/Serhii <serhii.atamaniuk@gmail.com>
2025-11-08 09:50:55 +0100

    inter-obs sect-3

---
+++ observer.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(".main_third--card");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+cards.forEach((card) => observer.observe(card));
